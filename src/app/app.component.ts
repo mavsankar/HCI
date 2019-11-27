b50e4d9f95ccc545e54414e1d3fb73a77ad9ce75
mavsankar <mavsankar@gmail.com>
2019-11-27 09:44:52 +0530

    Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class AppComponent  {
   title = 'Hci';
   searchKey:string;
-  constructor(private router:Router){}
+  constructor(public router:Router){}
   scroll(el: HTMLElement) {
     console.log(el);
     el.scrollIntoView();
